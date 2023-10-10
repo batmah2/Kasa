ab72e70be3a2ee96b6dd302b6ae1ad84d04ab32f
batmah2 <mahmoudisml3@gmail.com>
2023-10-10 13:29:27 +0200

    ajout underline sur les navLink

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,29 @@
+import { useState } from 'react'
 import Logo from '../../assets/LOGO.png'
 import { NavLink } from 'react-router-dom'
 
 export default function Header() {
+    const [isHomeActive, setIsHomeActive] = useState(false);
+    const [isAboutActive, setIsAboutActive] = useState(false);
+
+    const handleHomeClick = () => {
+        setIsHomeActive(!isHomeActive);
+        setIsAboutActive(false);
+    }
+
+    const handleAboutClick = () => {
+        setIsAboutActive(!isAboutActive);
+        setIsHomeActive(false);
+    }
+     
     return (
         <div className='navbar'>
             <img src={Logo} alt="logo kasa"/>
             <ul className='navbar_button'>
-                <NavLink className="navLink" to="/">Acceuil</NavLink>
-                <NavLink className="navLink" to="/a-propos">A propos</NavLink>
+                <NavLink className={`navLink ${isHomeActive ? "activeUnderline" : ""}`} to="/"  onClick={handleHomeClick}>Acceuil</NavLink>
+                <NavLink className={`navLink ${isAboutActive ? "activeAboutUnderline" : ""}`} to="/a-propos"  onClick={handleAboutClick}>A propos</NavLink>
             </ul>
         </div>
     )
-} 
+}
 
