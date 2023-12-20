4d17f8ab925c197e3fedcf854c00a6cf16cfd718
Mahmoud <123078179+batmah2@users.noreply.github.com>
2023-12-20 14:22:46 +0100

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,4 +10,7 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
+  "rules": {
+  "react/prop-types": "off"
+}
 }
